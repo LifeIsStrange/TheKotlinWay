2b1f5138abf83b7d8dd442a0320b217e870c6223
Stephane <stephanedubedat666@gmail.com>
2020-02-16 18:24:27 +0100

    add an exhaustive list of null safety features

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,11 @@
+fun reflectionsCanMakeAProgramAwareOfItself() {
+    println(::`strings can do a lot of things 👽`.name)
+}
 
+// todo https://kotlinlang.org/docs/reference/lambdas.html
 fun main() {
+    reflectionsCanMakeAProgramAwareOfItself()
+
     `strings can do a lot of things 👽`()
+    `Kotlin c'est pas null`()
 }
---
+++ src/safe.kt
@@ -0,0 +1,38 @@
+/*
+ Get rid of those pesky NullPointerExceptions, you know, The Billion Dollar Mistake
+*/
+
+// singleton are first class citizen
+object Singleton {
+    fun doNothing() {}
+    var nullableStr: String? = null
+}
+
+fun `Kotlin c'est pas null`() {
+    var output: String
+    // output = null   // Compilation error
+
+    // Kotlin protects you from mistakenly operating on nullable types
+
+    val name: String? = null    // Nullable type
+    // println(name.length())      // Compilation error
+
+    // btw functions can be nested
+    fun calculateTotal(obj: Any?) {
+        // And if you check a type is right, the compiler will auto-cast it for you AKA smartcast
+        if (obj is Singleton) {
+            obj.doNothing()
+            // with great power comes great responsibility
+            val lyingToTheCompiler: String = obj.nullableStr!!
+        }
+    }
+
+    val b: String? = null
+    // safe call AKA optional chaining
+    println(b?.length)
+
+    // safe cast
+    val aInt: Int? = b as? Int
+
+    val negativeLengthIfbIsNull = b?.length ?: -1
+}
